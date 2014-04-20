19bba7848c965e3c6d3b5f6f043e0c211d4984e4
Ahmed El Gabri <ahmed@gabri.me>
2014-04-20 22:43:46 +0400

    Gulp instead of Grunt

---
+++ Gulpfile.js
@@ -0,0 +1,37 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var csso         = require('gulp-csso');
+var rename       = require('gulp-rename');
+var exec         = require('child_process').exec;
+
+var config = {
+    scss : ['./scss/main.scss', './scss/extras/ie.scss'],
+    dest : './css'
+};
+
+
+gulp.task('sass', function(){
+    return gulp.src(config.scss)
+        .pipe(sass({
+            style        : 'compressed',
+            precision    : 3
+        }))
+        .pipe(autoprefixer('> 1%', 'last 1 version', 'ie 8', 'android 4', 'ios 6'))
+        .pipe(csso())
+        .pipe(rename({
+            suffix  : '.min',
+            extname : '.css'
+        }))
+        .pipe(gulp.dest(config.dest));
+});
+
+
+gulp.task('docs', function(){
+    exec('hologram');
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});

b2f9e04186ff4c4fa7fb8dba088fce6572452657
Ahmed El Gabri <ahmed@gabri.me>
2014-06-15 19:53:48 +0400

    Adding Gulp plumber

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,47 +1,46 @@
-var gulp         = require('gulp');
-var utils        = require('gulp-util');
-var sass         = require('gulp-ruby-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var csso         = require('gulp-csso');
-var rename       = require('gulp-rename');
+var gulp         = require('gulp'),
+    utils        = require('gulp-util'),
+    sass         = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    csso         = require('gulp-csso'),
+    plumber      = require('gulp-plumber'),
+    rename       = require('gulp-rename');
 
 var config = {
-    scss : ['./scss/main.scss', './scss/extras/ie.scss'],
-    dest : './css'
+    scss: [ './scss/main.scss','./scss/extras/ie.scss' ],
+    dest: './css'
 };
 
-
-gulp.task('sass', function(){
+gulp.task('sass', function() {
     return gulp.src(config.scss)
+        .pipe(plumber())
         .pipe(sass({
-            style        : 'compressed',
-            precision    : 3
+            style: 'compressed',
+            precision: 3
         }))
         .pipe(autoprefixer('> 1%', 'last 1 version', 'ie 8', 'android 4', 'ios 6'))
         .pipe(csso())
         .pipe(rename({
-            suffix  : '.min',
-            extname : '.css'
+            suffix: '.min',
+            extname: '.css'
         }))
         .pipe(gulp.dest(config.dest));
 });
-
 
 var feedback = function(event) {
   console.log( 'File '+ utils.colors.yellow(event.path) +' was '+ event.type );
 };
 
-gulp.task('watch', function(){
-    gulp.watch('./scss/**/*.scss', ['sass']).on('change', feedback);
+gulp.task('watch', function() {
+    gulp.watch('./scss/**/*.scss', [ 'sass' ]).on('change', feedback);
 });
 
-
-gulp.task('default', function(){
+gulp.task('default', function() {
     var tasks = Object.keys(gulp.tasks).sort();
 
     utils.log(utils.colors.yellow('You have the following tasks to run:'));
-    tasks.forEach(function(t){
-        if(t === 'default') return;
+    tasks.forEach(function(t) {
+        if ( t === 'default' ) return;
         utils.log(utils.colors.yellow('• gulp '+ t ));
     });
 });
